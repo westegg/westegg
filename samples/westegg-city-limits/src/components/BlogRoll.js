ebc19ff1b473923d643d54a2e5c16db0ef9305b0
Andrew Alexander <account+github@adrw.ch>
2019-03-28 10:45:29 -0400

    WIP

--- samples/westegg-city-limits/src/components/BlogRoll.js
+++ samples/westegg-city-limits/src/components/BlogRoll.js
@@ -1,35 +1,8 @@
-import { withStyles } from "@material-ui/core/styles"
-import GridList from "@material-ui/core/GridList"
-import GridListTile from "@material-ui/core/GridListTile"
-import GridListTileBar from "@material-ui/core/GridListTileBar"
 import React from "react"
+import PostCard from './PostCard'
 import PropTypes from "prop-types"
 import { Link, graphql, StaticQuery } from "gatsby"
 import Img from "gatsby-image"
-
-const styles = theme => ({
-  root: {
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "space-around",
-    overflow: "hidden",
-    backgroundColor: theme.palette.background.paper
-  },
-  gridList: {
-    width: 500,
-    height: 450,
-    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
-    transform: "translateZ(0)"
-  },
-  titleBar: {
-    background:
-      "linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, " +
-      "rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)"
-  },
-  icon: {
-    color: "white"
-  }
-})
 
 class BlogRoll extends React.Component {
   render() {
@@ -37,28 +10,45 @@ class BlogRoll extends React.Component {
     const { edges: posts } = data.allMarkdownRemark
 
     return (
+      <div className="PostSection">
+        {title && <h2 className="PostSection--Title">{title}</h2>}
+        {!!visiblePosts.length && (
+          <div className="PostSection--Grid">
+            {visiblePosts.map((post, index) => (
+              <PostCard key={post.title + index} {...post} />
+            ))}
+          </div>
+        )}
+        {showLoadMore && visiblePosts.length < posts.length && (
+          <div className="taCenter">
+            <button className="button" onClick={this.increaseLimit}>
+              {loadMoreTitle}
+            </button>
+          </div>
+        )}
+      </div>
       <GridList cellHeight={200} cols={2} spacing={1}>
-        {posts &&
-          posts.map(({ node: post }) => (
-            <GridListTile
-              key={`${post.frontmatter.title}${post.frontmatter.date}`}
-              cols={post.frontmatter.featured ? 2 : 1}
-              rows={post.frontmatter.featured ? 2 : 1}
-            >
-              <Link to={post.fields.slug}>
-                {post.frontmatter.image ? (
-                  <Img fluid={post.frontmatter.image.childImageSharp.fluid} />
-                ) : (
-                  <span />
-                )}
-                <GridListTileBar
-                  title={post.frontmatter.title}
-                  subtitle={<span>{post.frontmatter.date}</span>}
-                />
-              </Link>
-            </GridListTile>
-          ))}
-      </GridList>
+      {posts &&
+        posts.map(({ node: post }) => (
+          <GridListTile
+            key={`${post.frontmatter.title}${post.frontmatter.date}`}
+            cols={post.frontmatter.featured ? 2 : 1}
+            rows={post.frontmatter.featured ? 2 : 1}
+          >
+            <Link to={post.fields.slug}>
+              {post.frontmatter.image ? (
+                <Img fluid={post.frontmatter.image.childImageSharp.fluid} />
+              ) : (
+                <span />
+              )}
+              <GridListTileBar
+                title={post.frontmatter.title}
+                subtitle={<span>{post.frontmatter.date}</span>}
+              />
+            </Link>
+          </GridListTile>
+        ))}
+    </GridList>
     )
   }
 }
@@ -70,8 +60,6 @@ BlogRoll.propTypes = {
     })
   })
 }
-
-export const StyledBlogRoll = withStyles(styles)(BlogRoll)
 
 export default () => (
   <StaticQuery
@@ -105,6 +93,6 @@ export default () => (
         }
       }
     `}
-    render={(data, count) => <StyledBlogRoll data={data} count={count} />}
+    render={(data, count) => <BlogRoll data={data} count={count} />}
   />
 )
