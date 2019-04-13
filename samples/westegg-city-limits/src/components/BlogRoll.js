33411efa1ba099ced762293842a4b54934320463
Andrew Alexander <account+github@adrw.ch>
2019-04-13 10:50:07 -0400

    Remove material-ui

--- samples/westegg-city-limits/src/components/BlogRoll.js
+++ samples/westegg-city-limits/src/components/BlogRoll.js
@@ -1,35 +1,7 @@
-import { withStyles } from "@material-ui/core/styles"
-import GridList from "@material-ui/core/GridList"
-import GridListTile from "@material-ui/core/GridListTile"
-import GridListTileBar from "@material-ui/core/GridListTileBar"
 import React from "react"
 import PropTypes from "prop-types"
 import { Link, graphql, StaticQuery } from "gatsby"
 import Img from "gatsby-image"
-
-const styles = theme => ({
-  root: {
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "space-around",
-    overflow: "hidden",
-    backgroundColor: theme.palette.background.paper
-  },
-  gridList: {
-    width: 500,
-    height: 450,
-    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
-    transform: "translateZ(0)"
-  },
-  titleBar: {
-    background:
-      "linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, " +
-      "rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)"
-  },
-  icon: {
-    color: "white"
-  }
-})
 
 class BlogRoll extends React.Component {
   render() {
@@ -37,28 +9,40 @@ class BlogRoll extends React.Component {
     const { edges: posts } = data.allMarkdownRemark
 
     return (
-      <GridList cellHeight={200} cols={2} spacing={1}>
+      <div className="columns is-multiline">
         {posts &&
           posts.map(({ node: post }) => (
-            <GridListTile
-              key={`${post.frontmatter.title}${post.frontmatter.date}`}
-              cols={post.frontmatter.featured ? 2 : 1}
-              rows={post.frontmatter.featured ? 2 : 1}
-            >
-              <Link to={post.fields.slug}>
+            <div className="is-parent column is-6" key={post.id}>
+              <article className="tile is-child box notification">
                 {post.frontmatter.image ? (
                   <Img fluid={post.frontmatter.image.childImageSharp.fluid} />
                 ) : (
                   <span />
                 )}
-                <GridListTileBar
-                  title={post.frontmatter.title}
-                  subtitle={<span>{post.frontmatter.date}</span>}
-                />
-              </Link>
-            </GridListTile>
+                <p>
+                  <Link
+                    className="title has-text-primary is-size-4"
+                    to={post.fields.slug}
+                  >
+                    {post.frontmatter.title}
+                  </Link>
+                  <span> &bull; </span>
+                  <span className="subtitle is-size-5 is-block">
+                    {post.frontmatter.date}
+                  </span>
+                </p>
+                <p>
+                  {post.excerpt}
+                  <br />
+                  <br />
+                  <Link className="button" to={post.fields.slug}>
+                    Keep Reading →
+                  </Link>
+                </p>
+              </article>
+            </div>
           ))}
-      </GridList>
+      </div>
     )
   }
 }
@@ -70,8 +54,6 @@ BlogRoll.propTypes = {
     })
   })
 }
-
-export const StyledBlogRoll = withStyles(styles)(BlogRoll)
 
 export default () => (
   <StaticQuery
@@ -105,6 +87,6 @@ export default () => (
         }
       }
     `}
-    render={(data, count) => <StyledBlogRoll data={data} count={count} />}
+    render={(data, count) => <BlogRoll data={data} count={count} />}
   />
 )
