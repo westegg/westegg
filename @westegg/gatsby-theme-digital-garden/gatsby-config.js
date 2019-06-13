368dc43f0ae5c9fc35267ead45f9b0866e91c8ba
Andrew (Paradi) Alexander <gh@adrw.xyz>
2019-06-13 11:04:36 -0400

    Working digital theme starter (#75)

--- @westegg/gatsby-theme-digital-garden/gatsby-config.js
+++ @westegg/gatsby-theme-digital-garden/gatsby-config.js
@@ -1,3 +1,25 @@
-module.exports = {
-  plugins: [],
+module.exports = options => {
+  const {
+    mdx = true,
+    mdxLayouts = {},
+    notes = "notes",
+    siteMetadata = {
+      title: "Westegg Digital Garden"
+    }
+  } = options
+
+  return {
+    siteMetadata,
+    __experimentalThemes: [
+      {
+        resolve: "gatsby-theme-digital-garden",
+        options: {
+          mdx,
+          mdxLayouts,
+          notes
+        }
+      }
+    ],
+    plugins: []
+  }
 }
