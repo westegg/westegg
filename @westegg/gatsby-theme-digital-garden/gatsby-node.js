15b74ed47245f941085b3b1c382ef041d40e247f
Andrew Alexander <adrw@squareup.com>
2019-06-14 16:23:22 -0400

    Remove src/img unused directory

--- @westegg/gatsby-theme-digital-garden/gatsby-node.js
+++ @westegg/gatsby-theme-digital-garden/gatsby-node.js
@@ -1,5 +1,5 @@
-const fs = require("fs-extra")
-const path = require("path")
+const fs = require('fs-extra')
+const path = require('path')
 
 /* Check that the folders used by source-filesystem plugin exist.
  * If they do not, they will be created.
@@ -8,10 +8,7 @@ const path = require("path")
 exports.onPreBootstrap = ({ store, reporter }) => {
   const { program } = store.getState()
 
-  const dirs = [
-    path.join(program.directory, "src/img"),
-    path.join(program.directory, "static/img")
-  ]
+  const dirs = [path.join(program.directory, 'static/img')]
 
   dirs.forEach(dir => {
     reporter.log(`ensure ${dir} directory present`)
