46625d4e0495b43492fc73c99483ed72a7ed7863
Andrew Alexander <adrw@squareup.com>
2019-09-05 22:11:42 -0400

    Update to custom layout

--- @westegg/gatsby-theme-digital-garden/src/components/layout.js
+++ @westegg/gatsby-theme-digital-garden/src/components/layout.js
@@ -1,14 +1,31 @@
 import React from 'react'
+import { Global } from '@emotion/core'
+import { css } from 'theme-ui'
+import { Layout, Main, Container } from 'theme-ui/layout'
+import HeaderContainer from 'gatsby-theme-digital-garden/src/components/header-container'
+import { SEO } from 'gatsby-theme-digital-garden/src/components/seo'
 
-export default ({ children }) => (
-  <section
-    style={{
-      display: 'block',
-      maxWidth: '90%',
-      width: '650px',
-      margin: '2rem auto'
-    }}
-  >
-    {children}
-  </section>
+export default props => (
+  <>
+    <Global
+      styles={css({
+        '*': {
+          boxSizing: 'border-box'
+        },
+        body: {
+          margin: 0,
+          color: 'text',
+          bg: 'background',
+          fontFamily: 'body'
+        }
+      })}
+    />
+    <Layout>
+      <SEO />
+      <HeaderContainer />
+      <Main>
+        <Container>{props.children}</Container>
+      </Main>
+    </Layout>
+  </>
 )
