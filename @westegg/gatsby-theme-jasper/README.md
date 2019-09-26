1fa2aeeaba0ea5818fe5583f0af17030fddbfc56
Andrew Alexander <adrw@squareup.com>
2019-09-26 13:46:42 -0400

    Add Jasper theme

---
+++ @westegg/gatsby-theme-jasper/README.md
@@ -0,0 +1,5 @@
+# @westegg/gatsby-theme-jasper
+
+This theme extends [scttcper/gatsby-theme-casper](https://github.com/scttcper/gatsby-theme-casper) with further styling and works well with [@westegg/gatsby-theme-core](https://www.npmjs.com/package/@westegg/gatsby-theme-core).
+
+This Gatsby Theme lives in [Westegg](https://westegg.xyz/).
---
+++ starters/westegg-jasper/gatsby-config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  plugins: ['gatsby-theme-casper'],
+  siteMetadata: {
+    title: 'Ghost',
+    description: 'The professional publishing platform',
+    coverImage: 'img/blog-cover.jpg',
+    logo: 'content/avatars/ghost.png',
+    lang: 'en',
+    siteUrl: 'https://gatsby-casper.netlify.com',
+    facebook: 'https://www.facebook.com/ghost',
+    twitter: 'https://twitter.com/tryghost',
+    showSubscribe: true,
+    mailchimpAction:
+      'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
+    mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
+    mailchimpEmailFieldName: 'MERGE0',
+    googleSiteVerification: 'GoogleCode',
+    footer: 'is based on Gatsby Casper',
+    postsPerPage: 6
+  }
+}
