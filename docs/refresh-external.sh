975d78807ffe84a01df3866225ed874ac66a039c
Andrew Alexander <adrw@squareup.com>
2019-09-29 09:17:31 -0400

    Add NPM version badges to docs site

--- docs/refresh-external.sh
+++ docs/refresh-external.sh
@@ -11,5 +11,6 @@ cp -r "$THEMES-cache" "$THEMES"
 declare -a themes=("core" "digital-garden" "jasper" "misk")
 for theme in "${themes[@]}"; do
   echo "Updating docs/themes $theme"
+  cat "[![npm](https://img.shields.io/npm/v/@westegg/gatsby-theme-$theme.svg?label=@westegg/gatsby-theme-$theme)](https://www.npmjs.com/package/@westegg/gatsby-theme-$theme)" >> $THEMES/westegg-$theme.mdx
   cat $W/@westegg/gatsby-theme-$theme/README.md >> $THEMES/westegg-$theme.mdx
 done
