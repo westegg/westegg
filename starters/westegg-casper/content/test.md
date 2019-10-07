771936f9864aa96d7e186c8a50b4ae3b40e2e046
Andrew Alexander <adrw@squareup.com>
2019-10-06 21:32:21 -0400

    Fork gatsby-theme-casper

---
+++ starters/westegg-casper/content/test.md
@@ -0,0 +1,21 @@
+---
+layout: post
+title: Github Source
+image: avatars/ghost.png
+author: Ghost
+date: 2019-03-10T10:00:00.000Z
+draft: false
+tags:
+  - Source
+---
+
+hello
+
+```kotlin
+
+fun yo() = when {
+  "wat" -> "sup",
+  else -> null
+}
+
+```
---
+++ starters/westegg-casper/gatsby-config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: '@westegg/gatsby-theme-casper',
+      options: {
+        siteMetadata: {
+          title: 'Westegg Casper',
+          description: 'The mountainous publishing platform',
+          coverImage: 'img/blog-cover.jpg',
+          logo: 'avatars/ghost.png',
+          lang: 'en',
+          siteUrl: 'https://westegg.xyz',
+          facebook: 'https://www.facebook.com/ghost',
+          twitter: 'https://twitter.com/tryghost',
+          showSubscribe: true,
+          mailchimpAction:
+            'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
+          mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
+          mailchimpEmailFieldName: 'MERGE0',
+          googleSiteVerification: 'GoogleCode',
+          footer: 'lives in Westegg',
+          footerLink: 'https://westegg.xyz/',
+          footerShowRss: false,
+          postsPerPage: 6
+        }
+      }
+    }
+  ]
+}
